d96047c2b93ac5c7d3f9e1c63b4be8a86f9d9edb
Andrew McPherson <andrewmcp333@gmail.com>
2015-02-28 15:40:55 -0500

    Updated the link to repository in the scripts.

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
-console.log("If you're interested in our project, you should maybe consider");
-console.log("contributing to the repository at www.github.com/arcym/viditor!");
+console.log("If you're interested in our project, you should maybe consider")
+console.log("contributing to the repositories at http://www.github.com/viditor")
